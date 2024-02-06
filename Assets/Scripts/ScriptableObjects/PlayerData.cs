5ab9a4cebecc907e3490585ce979fcb5bb3b1f95
lfernandes14 <lfernandes@bradley.edu>
2024-02-06 14:29:27 -0600

    created a Player Data scriptable object
    player data SO contains variables for player name and player health

---
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : ScriptableObject
+{
+    [SerializeField] private string PlayerName;
+    [SerializeField] private int PlayerHealth;
+
+    public string PlayerName1 { get => PlayerName; set => PlayerName = value; }
+    public int PlayerHealth1 { get => PlayerHealth; set => PlayerHealth = value; }
+}
